52eef2ce333381140f4825bd339b18ce079beefa
luckyzhz <zhzwork@outlook.com>
2022-01-26 21:12:39 +0800

    缩短动画时长，setInterval 重新设回 1000

--- docs/timer.js
+++ docs/timer.js
@@ -162,7 +162,7 @@ function startTimer() {
       pauseTimer();
       restFlag = !restFlag;   // 更改休息状态
     }
-  }, 500);
+  }, 1000);
 
   running = !running;
   startButton.firstChild.innerHTML = "暂停";
